06dfcd9a97e74c8d87a5dda1f601b77d83d146a8
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-01-06 17:13:37 +0900

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,18 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    const arr = letter.split(' ');
+ 
+    const result = [];
+    for (let i of arr) {
+        result.push(morse[i]);
+    }
+    
+    return result.join('');
+}
