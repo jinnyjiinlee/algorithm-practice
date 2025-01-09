4bcd1f7770b5d6d987600c50b59f1ec5afffab57
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-01-09 09:59:57 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 9.80 ms, Memory: 35 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,16 +1,4 @@
 function solution(my_string, queries) {
-    let str = '';
-    let result = '';
-    
-    str = my_string;
-    
-    for (let [a, b] of queries) {
-        result = '';
-        result += str.slice(0, a);
-        result += str.split('').slice(a, b + 1).reverse().join('');
-        result += str.slice(b + 1);
-        str = result;
-    }
-
-    return result;
+    var answer = '';
+    return answer;
 }
