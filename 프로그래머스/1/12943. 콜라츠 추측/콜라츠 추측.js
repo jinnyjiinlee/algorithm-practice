1fd5156195fda857ac71c8329e52c8fd45d8c8e0
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-01-16 14:20:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -9,15 +9,9 @@ function solution(num) {
     
     if (num === 1) return 0;
     
-    while (num !== 1) {
+    while (num !== 1 && count < 500) {
         // 짝수라면? 
-        if (num % 2 === 0) {
-            num = num / 2;
-        } 
-        // 홀수라면? 
-        else {
-            num = num * 3 + 1;
-        }
+        num % 2 === 0 ? num = num / 2 : num = num * 3 + 1;
         count += 1;
     }
     
