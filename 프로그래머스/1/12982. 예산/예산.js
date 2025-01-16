24539e05ad09a4bb95530008b092a50fa03a90e5
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-01-16 14:55:10 +0900

    [level 1] Title: 예산, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.js
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -3,7 +3,6 @@ function solution(d, budget) {
     let count = 0;
     
     let dSorted = d.sort((a, b) => a - b);
-    console.log(dSorted)
     
     for (let i = 0; i < dSorted.length; i++) {
         if (dSorted[i] <= left) {
