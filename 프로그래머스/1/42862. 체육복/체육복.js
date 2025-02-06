5b17bc10813fc452e6a4c951603529896bf8beba
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-06 15:56:29 +0900

    [level 1] Title: 체육복, Time: 0.31 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,18 +1,37 @@
 function solution(n, lost, reserve) {
-    let canCount = 0; 
-
-    lost.forEach(v => {
-        let a = v - 1;
-        let b = v + 1;
-        
-        if (reserve.includes(a) && (!lost.includes(a))) {
-            canCount++;
-            reserve.splice(reserve.indexOf(a), 1);
-        } else if ((reserve.includes(b) && (!lost.includes(b)))) {
-            canCount++;
-            reserve.splice(reserve.indexOf(b), 1);   
-        }
-    })
+    let answer = 0;
+    let students = {};
     
-    return n - lost.length + canCount;
+    for (let i = 1; i <= n; i++) {
+        students[i] = 1;
+    }
+    
+    // 잃어버린 학생들 처리
+    lost.forEach(v => students[v] -= 1);
+    
+    // 여러 학생들 처리
+    reserve.forEach(v => students[v] += 1);
+    
+    // students학생들 조회 
+    for (let i = 1; i <= n; i++) {
+        // 내가 2개 있고 앞 사람 없으면 빌려주기
+         if ((students[i] === 2) && students[i - 1] === 0) {
+             students[i - 1] += 1;
+             students[i] -= 1;
+         }
+        
+        // 내가 2개 있고 뒷 사람 없으면 빌려주기 
+         if ((students[i] === 2) && students[i + 1] === 0) {
+             students[i + 1] += 1;
+             students[i] -= 1;
+         }
+    }
+    
+    for (let key in students) {
+        if (students[key] >= 1) {
+            answer++;
+        }
+    }
+    
+    return answer;
 }
