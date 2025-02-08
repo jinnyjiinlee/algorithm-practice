66fa175de8e68c2e8f5ad1708579a66a57fac58c
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-08 22:02:13 +0900

    [level 2] Title: 올바른 괄호, Time: 4.42 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,11 +1,11 @@
 function solution(s){
-    let count = 0;
+    let stack = 0;
     
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(') count += 1;                        
-        if (s[i] === ')') count -= 1; 
-        if (count < 0) return false;
+        if (s[i] === '(') stack++;
+        if (s[i] === ')') stack--;
+        if (stack < 0 ) return false;        
     }
-
-    return count === 0 ? true : false;    
+    
+    return stack === 0 ? true : false; 
 }
