fed9c4adb1fedc032e4ded4abbac77d35933d456
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-08 22:49:06 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,12 +1,13 @@
 function solution(array, commands) {
-    let result = [];
+    let answer = [];
     
-    commands.forEach(v => {
-        let arr = [];
-        arr = array.slice(v[0] - 1, v[1]);
-        arr.sort((a, b) => a - b);
-        result.push(arr[v[2] - 1])
-    })
-
-    return result;
+    for (let i of commands) {
+        let a = array.slice(i[0] - 1, i[1]);
+        a = a.sort((a, b) => a - b);
+        
+        let b = a[i[2] - 1];
+        answer.push(b);
+    }
+    
+    return answer;
 }
