f863624265d01fd364c68475cbfa79520ab1911e
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-08 22:27:34 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 65.26 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,22 +1,19 @@
 function solution(participant, completion) {
-    let hash = {}
+    let hash = {};
     
-    // hash테이블에 참여자들 이름 만들기 
     for (let i of participant) {
         hash[i] = 0;
     }
     
-    // hash테이블에 참여자들 이름과 숫자 1 넣기
     for (let i of participant) {
-        hash[i] += 1;
+        hash[i]++;
     }
     
-    // 완주한 사람들에게는 숫자 -1 하기
     for (let i of completion) {
         if (hash[i] > 0) hash[i]--;    
     }
-    
-    for (let k in hash) {
-        if (hash[k] > 0) return k; 
+
+    for (let k in hash ) {
+        if (hash[k] > 0) return k;
     }
 }
