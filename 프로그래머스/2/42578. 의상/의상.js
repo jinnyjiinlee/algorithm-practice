a30e0a78d4dbf67da05772b6dc68b595de0a1f2e
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-10 00:16:12 +0900

    [level 2] Title: 의상, Time: 0.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,20 +1,12 @@
 function solution(clothes) {
-    let hash = {}
+    const hash = {};
     
-    clothes.forEach(v => {
-        hash[v[1]] = 0;
-    })
-    
-    clothes.forEach(v => {
-        for (let key in hash) {
-            if (v[1] === key) hash[key]++;
+    for (let ele of clothes) {
+        if (!hash[ele[1]]) {
+            hash[ele[1]] = 0;
         }
-    })
-    
-    let arr = Object.values(hash);
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] += 1;
-    }
-    
-    return arr.reduce((a, b) => a * b, 1) - 1;
+        hash[ele[1]] += 1;
+    }    
+      
+    return Object.values(hash).reduce((a, b) => a * (b + 1), 1) - 1;
 }
