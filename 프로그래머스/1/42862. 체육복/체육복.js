50c59bf46632d82d2857849a41856bbcc4179af6
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-10 00:44:18 +0900

    [level 1] Title: 체육복, Time: 0.30 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,36 +1,37 @@
 function solution(n, lost, reserve) {
     let answer = 0;
-    let students = {};
+    let hash = {};
     
     for (let i = 1; i <= n; i++) {
-        students[i] = 1;
+        hash[i] = 1;
     }
     
-    // 잃어버린 학생들 처리
-    lost.forEach(v => students[v] -= 1);
-    
-    // 여러 학생들 처리
-    reserve.forEach(v => students[v] += 1);
-    
-    // students학생들 조회 
-    for (let i = 1; i <= n; i++) {
-        // 내가 2개 있고 앞 사람 없으면 빌려주기
-         if ((students[i] === 2) && students[i - 1] === 0) {
-             students[i - 1] += 1;
-             students[i] -= 1;
-         }
-        
-        // 내가 2개 있고 뒷 사람 없으면 빌려주기 
-         if ((students[i] === 2) && students[i + 1] === 0) {
-             students[i + 1] += 1;
-             students[i] -= 1;
-         }
-    }
-    
-    for (let key in students) {
-        if (students[key] >= 1) {
-            answer++;
+    lost.forEach(v => {
+        for (let key in hash) {
+            if (v === Number(key)) hash[key]--;
         }
+    })
+    
+    reserve.forEach(v => {
+        for (let key in hash) {
+           if (v === Number(key)) hash[key]++;
+        }
+    })
+    
+    for (let i = 1; i <= n; i++) {
+        if((hash[i] === 2) && hash[i - 1] === 0) {
+            hash[i - 1]++;
+            hash[i]--;
+        }
+        
+        if((hash[i] === 2) && hash[i + 1] === 0) {
+            hash[i + 1]++;
+            hash[i]--;
+        }
+    }
+    
+    for (let key in hash) {
+        if (hash[key] >= 1) answer++;
     }
     
     return answer;
