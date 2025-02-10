8c9196893a66476e38b094b3ae7cf3745740f823
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-10 15:15:18 +0900

    [level 2] Title: 가장 큰 수, Time: 169.82 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,9 +1,8 @@
 function solution(numbers) {
     let answer = numbers
-            .map(v => v + '')
-            .sort((a, b) => (b + a) * 1 - (a + b) * 1)
-            .join('');
+            .map(num => num + '')
+            .sort((a, b) => ((b + a) * 1) - ((a + b) * 1))
+            .join('')
     
-    return answer[0] === '0'? '0' : answer;
+    return answer[0] === '0' ? '0' : answer;
 }
-
