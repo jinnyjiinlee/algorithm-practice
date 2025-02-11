eb40d9e5e43a6bec2010d990b9931db69f888786
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-11 14:17:13 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,23 +1,20 @@
 function solution(numbers, direction) {
-    // numbers: 정수가 담긴 배열
-    // direction: left or right
-    
     if (direction === 'right') {
-        // 마지막 배열 저장
-        const last = numbers[numbers.length - 1];
-        // 마지막 배열 삭제
+        // 마지막 원소 저장
+        let a = numbers[numbers.length - 1];
+        // 마지막 원소 삭제
         numbers.pop();
-        // 마지막 배열 저장된 것을 맨 처음 배열로 넣기
-        numbers.unshift(last);
-    }
-    
+        // 맨 앞으로 보내기
+        numbers.unshift(a);
+    } 
+
     if (direction === 'left') {
-        // 맨 처음 배열 저장
-        const first = numbers[0]
-        // 맨 처음 배열 삭제
+        // 첫 번째 원소 저장
+        let b = numbers[0];
+        // 첫 번째 원소 삭제
         numbers.shift();
-        // 맨 처음 배열 저장된 것을 마지막 배열로 넣기 
-        numbers.push(first);
+        // 맨 뒤로 보낵기
+        numbers.push(b);
     }
     
     return numbers;
