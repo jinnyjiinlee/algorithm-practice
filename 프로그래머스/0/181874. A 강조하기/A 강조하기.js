fbafa9ed75eeaa0cc54198a30fd4db247d14ea4c
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-11 13:44:09 +0900

    [level 0] Title: A 강조하기, Time: 0.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -1,14 +1,12 @@
 function solution(myString) {
-    // myString: 문자열
-    // 알파벳 a가 등장하면 전부 A로 변환
-    // A아닌 모든 대문자 알파벳 -> 소문자로 변환
+    // 알파벳 a등장 => 전부 A로
+    // A가 아닌 모든 대문자 알파벳 -> 소문자로
+    return myString.split('')
+        .map(v => v.toLowerCase())
+        .map(v => {
+            if (v === 'a') {
+                return 'A'
+            } else return v })
+        .join('')
     
-    return [...myString].map(v => {
-        if (v === 'a') return 'A';
-        if ((v !== 'A') && (v.toUpperCase() === v)) {
-            return v.toLowerCase();
-        } else {
-            return v;
-        }
-    }).join('');
 }
