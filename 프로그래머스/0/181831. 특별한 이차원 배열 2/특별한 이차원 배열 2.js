656b010468c02f6a8ebb131133d9dd4bd3a1f508
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-11 13:59:15 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 2.37 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -1,3 +1,17 @@
+// n * n 크기의 이차원 배열 arr이 매개변수로 주어질 때,
+// arr이 다음을 만족하면 1 / 아니라면 0 을 ㄱeturn gofk 
 function solution(arr) {
-    return arr.every((r, i) => r.every((_, j) => arr[i][j] === arr[j][i])) ? 1 : 0;
+    let count = 0;
+    let length = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        length += arr[i].length;
+
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] === arr[j][i]) count++;
+        }
+    }
+        
+    return count === length ? 1 : 0;
+
 }
