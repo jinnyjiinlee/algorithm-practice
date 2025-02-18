5d2c5a9b47bc828580c484a619ac000588660899
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-18 10:26:40 +0900

    [level 1] Title: 폰켓몬, Time: 9306.93 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,29 +1,9 @@
 function solution(nums) {
     let 포켓몬 = {};
     
-    // 포켓몬 해시에 초기값 0으로 세팅하기 
     for (let i = 0; i < nums.length; i++) {
         포켓몬[nums[i]] = 0;
     }
     
-    for (let number of nums) {
-        // 포켓몬 해시값 돌기
-        for (let key in 포켓몬) {
-            if (Number(key) === number) 포켓몬[key]++; 
-        }
-    }
-    
-    let 포켓몬종류 = Object.keys(포켓몬).length;
-    let 포켓몬의수 =  Object.values(포켓몬).reduce((a, b) => a + b, 0);
-    let 가져가도되는포켓몬수 = 포켓몬의수 / 2;
-    
-    console.log('포켓몬 종류: ', 포켓몬종류)
-    console.log('포켓몬 수: ', 포켓몬의수)
-    console.log('가져가도되는포켓몬수: ', 가져가도되는포켓몬수)
-    
-    // 더 큰게 아니면? 
-    return 포켓몬종류 >= 가져가도되는포켓몬수 ? 가져가도되는포켓몬수 : 포켓몬종류;
-    
-
-    
+    return 포켓몬;
 }
