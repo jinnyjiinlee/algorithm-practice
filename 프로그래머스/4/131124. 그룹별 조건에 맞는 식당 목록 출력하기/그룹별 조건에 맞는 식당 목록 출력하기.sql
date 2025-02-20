5bc056b47c0b8cad3cf7fd2bfbf8fc13ec998727
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-20 11:31:54 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,22 +1,30 @@
-# 고객의 정보를 담은 MEMBER_PROFILE - 회원 id, 이름, 연락처, 성별, 생년월일 
-# 식당 리뷰 정보를 담은 REST_REVIEW - 리뷰 id, 식당 id, 회원 id, 점수, 리뷰, 텍스트, 리뷰 작성일 
+# MEMBER_PROFILE: 고객의 정보를 담은 테이블
+# - 회원 id, 이름, 연락처, 성별, 생년월일
 
-# 리뷰를 가장 많이 작성한 회원의 리뷰를 조회 
-# 결과? 리뷰 작성일 오름 차순 / 리뷰 텍스트 오름 차순 
+# REST_REVIEW: 식당 리뷰 담은 테이블 
+# - 리뷰id, 식당id, 회원id, 점슈, 텍스ㅡ ,작성일 
+
+# 리뷰를 가장 많이 작성한 ㅇ회원 
+
 
 SELECT 
-    MP.MEMBER_NAME,
-    RR.REVIEW_TEXT,
-    DATE_FORMAT(RR.REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
-FROM MEMBER_PROFILE MP
-JOIN REST_REVIEW RR ON MP.MEMBER_ID = RR.MEMBER_ID
-WHERE RR.MEMBER_ID = (
-    SELECT MEMBER_ID
-    FROM REST_REVIEW
+    MP.MEMBER_NAME 
+    , REVIEW_TEXT
+    , DATE_FORMAT(REVIEW_DATE, '%Y-%m-%d') AS REVIEW_DATE
+
+    # , COUNT(*) 
+FROM REST_REVIEW RV
+
+JOIN MEMBER_PROFILE MP ON MP.MEMBER_ID = RV.MEMBER_ID
+
+WHERE MP.MEMBER_ID = (
+    SELECT 
+        MEMBER_ID 
+    FROM 
+        REST_REVIEW
     GROUP BY MEMBER_ID
     ORDER BY COUNT(*) DESC
     LIMIT 1
 )
-ORDER BY 
-    RR.REVIEW_DATE ASC,
-    RR.REVIEW_TEXT ASC;
+
+ORDER BY REVIEW_DATE, REVIEW_TEXT
