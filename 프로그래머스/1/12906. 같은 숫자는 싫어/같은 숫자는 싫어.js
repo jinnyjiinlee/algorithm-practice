286a4faa933218f768bf4d181124fd7c69ef4383
Jiin Lee (Jinny) <163097586+jinnyjiinlee@users.noreply.github.com>
2025-02-20 10:01:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 46.35 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,10 @@
 function solution(arr) {
-
     let stack = [];
     
     arr.forEach(v => {
-        if (((stack.length === 0) || (stack[stack.length - 1] !== v))) {
-            stack.push(v);
-        }
-    });
+        if (stack.length === 0) stack.push(v);
+        if (stack[stack.length - 1] !== v) stack.push(v);
+    })
     
-    return stack;
+    return stack
 }
